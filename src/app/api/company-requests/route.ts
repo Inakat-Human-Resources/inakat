04816860af54f8890f2834f673fca155391f8b87
Guillermo Sanchez <guillermo.sanchezy@gmail.com>
2026-01-11 17:21:30 -0600

    Add word-extractor for native .doc support

--- src/app/api/company-requests/route.ts
+++ src/app/api/company-requests/route.ts
@@ -112,7 +112,6 @@ export async function POST(request: Request) {
           apellidoMaterno,
           role: "company",
           isActive: true,
-          emailVerified: false,
           companyRequest: {
             connect: { id: companyRequest.id }
           }
